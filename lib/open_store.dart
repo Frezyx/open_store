3206c39b4fae7d7fb046cca74d28c0d84ac9cd2f
Stanislav Ilin <youngfrezyx@gmail.com>
2021-05-24 17:09:58 +0500

    Init package

---
+++ lib/open_store.dart
@@ -0,0 +1,54 @@
+library open_store;
+
+import 'dart:io';
+
+import 'package:flutter/services.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+import 'src/eceptions/common.dart';
+
+class OpenStore {
+  OpenStore._();
+
+  static final OpenStore _instance = OpenStore._();
+  static OpenStore get instance => _instance;
+
+  Future<void> open({
+    required String appStoreId,
+    required String androidAppBundleId,
+  }) async {
+    if (Platform.isIOS) {
+      await _openAppStore(appStoreId);
+    } else if (Platform.isAndroid) {
+      await _openGooglePlay(appStoreId);
+    } else
+      throw PlatformException(code: 'Platform not supported');
+  }
+
+  Future<void> _openAppStore(String androidAppBundleId) async {
+    try {
+      final pageUri = 'https://apps.apple.com/app/id$androidAppBundleId';
+      _opeenCommon(pageUri);
+    } catch (_) {
+      rethrow;
+    }
+  }
+
+  Future<void> _openGooglePlay(String androidAppBundleId) async {
+    try {
+      final pageUri =
+          'https://play.google.com/store/apps/details?id=$androidAppBundleId';
+      _opeenCommon(pageUri);
+    } catch (_) {
+      rethrow;
+    }
+  }
+
+  Future<void> _opeenCommon(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw CantLaunchPageException('Could not launch $url');
+    }
+  }
+}
---
+++ lib/src/eceptions/common.dart
@@ -0,0 +1,9 @@
+class OpenStoreException implements Exception {
+  String cause;
+  OpenStoreException(this.cause);
+}
+
+/// This exception was thrown when page in store can't be launchd
+class CantLaunchPageException extends OpenStoreException {
+  CantLaunchPageException(String cause) : super(cause);
+}
